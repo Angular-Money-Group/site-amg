3998d1d8c2eeab4f638ecb3e3bcb1eea61f22b78
augustopsalgado <42944134+augustopsalgado@users.noreply.github.com>
2022-10-06 17:13:16 -0300

    update git

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  isMenuOpen = false;
   constructor(private router: Router) { }
 
   ngOnInit(): void {
