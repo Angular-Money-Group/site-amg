4ef4ed021150dd541e35062967e0e1150fb839f4
augustopsalgado <42944134+augustopsalgado@users.noreply.github.com>
2022-10-17 10:09:26 -0300

    responsividade em telas maiores

--- src/app/core/contact-us/contact-us.component.ts
+++ src/app/core/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent implements OnInit {
+  submitted = false;
+  contactForm! : FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.contactForm = new FormGroup({
+    email: new FormControl('',[Validators.required,Validators.email] ),
+    phoneNumber:new FormControl('',[Validators.required,Validators.pattern('^1\d\d(\d\d)?$|^0800 ?\d{3} ?\d{4}$|^(\(0?([1-9a-zA-Z][0-9a-zA-Z])?[1-9]\d\) ?|0?([1-9a-zA-Z][0-9a-zA-Z])?[1-9]\d[ .-]?)?(9|9[ .-])?[2-9]\d{3}[ .-]?\d{4}$'),]),
+    message: new FormControl('',Validators.required),
+  })
   }
+  get email(){
+    return this.contactForm.get('email')!;
+  }
+  get phoneNumber(){
+    return this.contactForm.get('phoneNumber')!;
+  }
+  get message(){
+    return this.contactForm.get('message')!;
+  }
+
+  onSubmit(){
+    //console.log(this.contactForm.value)
+    this.submitted = true;
+    if (this.contactForm.invalid) {
+      return;
+  }
+
+   }
 
 }
