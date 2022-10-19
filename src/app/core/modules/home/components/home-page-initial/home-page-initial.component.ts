f3d9fd81038272f8ef7c334d956f7d2bc49e1268
augustopsalgado <42944134+augustopsalgado@users.noreply.github.com>
2022-10-19 16:26:41 -0300

    feat(AMG-002): correção de bugs

--- src/app/core/modules/home/components/home-page-initial/home-page-initial.component.ts
+++ src/app/core/modules/home/components/home-page-initial/home-page-initial.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DefaultService } from '../../../../services/default-service/default.service';
 
 @Component({
   selector: 'app-home-page-initial',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageInitialComponent implements OnInit {
 
-  constructor() { }
+  constructor(public defaultService:DefaultService) { }
 
   ngOnInit(): void {
   }
--- src/app/core/modules/home/components/home-page-we-are/home-page-we-are.component.ts
+++ src/app/core/modules/home/components/home-page-we-are/home-page-we-are.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DefaultService } from '../../../../services/default-service/default.service';
 
 @Component({
   selector: 'app-home-page-we-are',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageWeAreComponent implements OnInit {
 
-  constructor() { }
+  constructor(public defaultService:DefaultService) { }
 
   ngOnInit(): void {
   }
