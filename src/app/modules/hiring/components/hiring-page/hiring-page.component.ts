fbddcd0a65b368c802219e0b73b43304ecc39867
augustopsalgado <42944134+augustopsalgado@users.noreply.github.com>
2022-11-08 09:42:12 -0300

    Correção do footer e inserção das meta tags

--- src/app/modules/hiring/components/hiring-page/hiring-page.component.ts
+++ src/app/modules/hiring/components/hiring-page/hiring-page.component.ts
@@ -1,6 +1,7 @@
 import { hiringTexts } from '../../models/texts';
 import { Component, OnInit, EventEmitter } from '@angular/core';
 import { DefaultService } from 'src/app/core/services/default.service';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-hiring-page',
@@ -13,8 +14,14 @@ export class HiringPageComponent implements OnInit {
 
 
   constructor(
-    public defaultService:DefaultService
-  ) { }
+    public defaultService:DefaultService,
+    private titleService: Title,
+    private meta:Meta
+  ) {
+    this.titleService.setTitle('Trabalhe Conosco');
+    this.meta.addTag({name: 'description', content:''});
+    this.meta.addTag({name: 'keywords', content:''});
+  }
 
   ngOnInit(): void {
   }
